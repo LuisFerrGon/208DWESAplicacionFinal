659706a02f80255c796efa193144a5b933527b70
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2025-01-21 09:12:38 +0100

    21/01

---
+++ model/UsuarioDB.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * @author Luis Ferreras González
+     * @version 1.0.0 Fecha última modificación del archivo: 20/01/2025
+     * @since 1.0.0
+     */
+
+    /**
+     * @author Luis Ferreras González
+     * @version 1.0.0 Fecha última modificación: 20/01/2025
+     * @since 1.0.0
+     */
+    interface UsuarioDB{
+        public static function validarUsuario($codigoUsuario, $contrasenaUsuario);
+    }
+?>
