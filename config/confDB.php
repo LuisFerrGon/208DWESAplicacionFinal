92e4f02adac36a34476c0a430917f61db59bce6f
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2025-01-22 09:37:13 +0100

    22/01 2 	Cambio a desarrollo

--- config/confDB.php
+++ config/confDB.php
@@ -2,16 +2,16 @@
     /**
      * Fichero de configuración de la base de datos
      * @author Luis Ferreras González
-     * @version 1.0.0 Fecha última modificación del archivo: 20/01/2025
+     * @version 1.0.0 Fecha última modificación del archivo: 22/01/2025
      * @since 1.0.0
      */
 
     //Datos en entorno desarrollo de clase
-//        const SERVIDOR="mysql:host=daw208.isauces.local;dbname=DB208DWESAppFinal";
-//        const USUARIO="user208DWESAppFinal";
-//        const CONTRASENA="paso";
+        const SERVIDOR="mysql:host=daw208.isauces.local;dbname=DB208DWESAppFinal";
+        const USUARIO="user208DWESAppFinal";
+        const CONTRASENA="paso";
     //Datos en entorno explotacion
-    const SERVIDOR="mysql:host=localhost:3306;dbname=DB208DWES";
-    const USUARIO="user208DWES";
-    const CONTRASENA="paso1234321osap";
+//    const SERVIDOR="mysql:host=localhost:3306;dbname=DB208DWES";
+//    const USUARIO="user208DWES";
+//    const CONTRASENA="paso1234321osap";
 ?>
